152ffccc4a72fa5a40e0a82b3ae250e513ff0920
Malakelis <awgeorgis@gmail.com>
2024-02-04 18:14:10 -0800

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,2 +1,72 @@
-def func(asdfad):
-    
+import math
+
+def a(n):
+    count = 0
+    for i in range(0, n):   #i: 0 to n-1
+        j = i
+        while j > 0:
+            count += 1 #count how many times print(i, j) runs
+            j = j - 2
+    return count
+
+def formulaA(n):
+    if n % 2 == 0:
+        return (n/2) * (n/2)
+    else:
+        return math.floor((n/2)) * (n+1)/2
+
+def b(n):
+    count = 0
+    for i in range(0, n):   # i: 0 to n-1
+        for j in range(0, 2*i):
+            count += 1
+    return count
+
+def formulaB(n):
+    return n*(n-1)
+
+
+def c(n):
+    count = 0
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            for k in range(1, n+1):
+                if i != j and i != k and k != j:
+                    count += 1
+    return count
+
+def formulaC(n):
+    return n*(n-1)*(n-2)
+
+n = 10
+
+testCases = set()
+
+for i in range(1,50):
+    testCases.add(i)
+
+
+
+def checkA(n):
+    if a(n) == formulaA(n):
+        return True
+
+def checkB(n):
+    if b(n) == formulaB(n):
+        return True
+
+def checkC(n):
+    if c(n) == formulaC(n):
+        return True
+
+def conclusiveCheck():
+    check = True
+    for i in testCases:
+        if not (checkA(i) and checkB(i) and checkC(i)):
+           check = False
+    if check == True:
+        print("Passed testcases")
+    else:
+        print("failed")
+        
+conclusiveCheck()
