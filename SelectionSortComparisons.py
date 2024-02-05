88bbec7c89059cb3a7ef50d4386a4cc29c9154a0
Malakelis <awgeorgis@gmail.com>
2024-02-04 23:06:42 -0800

    selection sort comparison check

---
+++ SelectionSortComparisons.py
@@ -0,0 +1,16 @@
+def SelectionSort(arr):
+    count = 0
+    n = len(arr) - 1
+    for i in range(0,n):
+        m = i
+        for j in range(i+1, n+1):
+            count += 1
+            if arr[j] < arr[m]:
+                m = j
+            if i != m:
+                arr[i], arr[m] = arr[m], arr[i]
+    return count
+
+array = [12,3,1,24,6,8]
+
+print("length array " + str(len(array)) + " count of comparisons " +  str(SelectionSort(array)))
