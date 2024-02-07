2ce227b5b62f567b46c9a65d695b3bcc185fcef3
Malakelis <awgeorgis@gmail.com>
2024-02-06 17:22:56 -0800

    val tracing

--- q4.py
+++ q4.py
@@ -2,6 +2,7 @@ def FunctionSolver(f, n, E):
     low = 0
     high = n
     val = (high + low)/2
+    print("Val is now " + str(val))
     while high - low > 2*E:
         if f(val) == 0:
             return val
@@ -14,5 +15,8 @@ def FunctionSolver(f, n, E):
 
 def f(val):
     return pow(2, val) - 2*val
+
+def f1(val):
+    return pow()
 
 print(FunctionSolver(f, 2, 0.03125))
